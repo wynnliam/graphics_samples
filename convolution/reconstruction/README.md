52f6e94728dac97e8f57c63bed78ed89bf6c7903
Illtyd Wynn <illtyd@mail.wynnilltyd.xyz>
2025-01-21 13:54:03 -0800

    Added reconstruction demo

---
+++ convolution/reconstruction/README.md
@@ -0,0 +1,9 @@
+# Reconstruction
+This sample is taken from the following source:
+
+*Chapter 9 - Signal Processing, Fundamentals of Computer Graphics - 4th Edition* by Marschner and Shirley
+
+*Reconstruction* is the process of taking a discrete sequence and making it continuous by applying a continuous
+filter to it. This is most valuable when paired with *sampling*: taking a continuous function and discretizing
+it; in a real-world application you may have sampled a song and stored it in a discrete format, and want to
+replay the song by reconstructing it.
---
+++ convolution/reconstruction/reconstruct.py
@@ -0,0 +1,40 @@
+# Liam Wynn, 1-12-2025, Graphics Samples: Reconstruction
+
+#
+# Defines a simple program that performs discrete-continuous convolution.
+# Specifically, we looking at taking a discrete sequence a and making it
+# continuou using a gaussian filter f(x) = e^(-x^2).
+#
+
+import numpy as np
+
+#
+# Defines a simple discrete sequence a = [1, 2, 3, 4]
+#
+
+a = [1, 2, 3, 4]
+
+#
+# Defines a simple continuous filter - gaussian.
+#
+
+def f(k):
+  return np.exp(-k ** 2)
+
+def reconstruct(a, f, x):
+  s = 0
+  for n in range(len(a)):
+    s += a[n] * f(x - n)
+
+  return s
+
+#
+# So we have a discrete sequence a = [1, 2, 3, 4]. If we are making
+# it continuous, the benefit is we can ask questions like, "what is
+# the value of a[1.5]? Of course, a[1.5] doesn't exist, but we
+# reconstruct it by using a filter f to compute it.
+#
+ 
+x = 1.5
+result = reconstruct(a, f, x)
+print(f"Reconstruction result at x = {x}: {result}")
